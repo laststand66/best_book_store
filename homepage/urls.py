431bd5c86f4cecf0f1b2811a8cc476a50f1d4c90
laststand66 <aleksds8466@gmail.com>
2020-01-14 17:27:42 +0300

    created registration without message sending

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
-from .views import index, contacts, BBLoginView, profile, BBLogoutView
+from .views import index, contacts, BBLoginView, profile, BBLogoutView, ChangeUserInfoView, \
+    BBPasswordChangeView, RegisterUserView, RegisterDoneView
 
 app_name = 'homepage'
 urlpatterns = [
+    path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
+    path('accounts/register/', RegisterUserView.as_view(), name='register'),
+    path('accounts/password/change/', BBPasswordChangeView.as_view(), name='password_change'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import UpdateView, CreateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+from django.views.generic.base import TemplateView
+
+
+from .models import MainUser
+from .forms import ChangeUserInfoForm, RegisterUserForm
 # Create your views here.
 # Контроллер-функция, которая возвращает нам базовую страницу из папки шаблонов
 
@@ -23,3 +32,35 @@ def profile(request):
 class BBLogoutView(LoginRequiredMixin, LogoutView):
     template_name = 'homepage/profile_tmp/logout.html'
 
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = MainUser
+    template_name = 'homepage/profile_tmp/change_user_info.html'
+    form_class = ChangeUserInfoForm
+    success_url = reverse_lazy('homepage:profile')
+    success_message = 'Личные данные пользователя изменены'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
+
+
+class BBPasswordChangeView(SuccessMessageMixin, LoginRequiredMixin, PasswordChangeView):
+    template_name = 'homepage/profile_tmp/password_change.html'
+    success_url = reverse_lazy('homepage:profile')
+    success_message = 'Пароль изменён успешно'
+
+
+class RegisterUserView(CreateView):
+    models = MainUser
+    template_name = 'homepage/profile_tmp/register_user.html'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('homepage:register_done')
+
+
+class RegisterDoneView(TemplateView):
+    template_name = 'homepage/profile_tmp/register_done.html'
